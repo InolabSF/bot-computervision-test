8a16278f59f8256ad067fbfeb11a526115d6f32f
Kazuya <fjky9132@gmail.com>
2017-10-12 16:50:02 -0700

    Initialize repository

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,48 +3,44 @@ class HomeController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def line_test
-    print JSON.dump(params)
+    # print JSON.dump(params)
 
       # messaging = params['events'][0]['type']
       # raise "messaging is needed" unless messaging
-
+    begin
       user_id = params['events'][0]['source']['userId']
-      print user_id
-      print 'done'
+      raise "line user id is needed" unless user_id
+      text = params['events'][0]['message']['text']
+      raise "text is needed" unless user_id
+      # messaging = params['events'][0]['type']
+      # raise "messaging is needed" unless messaging
 
-      # raise "line user id is needed" unless user_id
+    rescue => e
+      json = { :application_code => 400, :message => e.message}
+      render :json => json, :status => 400
+      return
+    end
+    #
+    lineUrl = "https://api.line.me/v2/bot/message/push"
 
-    # rescue => e
-    #   json = { :application_code => 400, :message => e.message}
-    #   render :json => json, :status => 400
-    #   return
-    # end
-    #
-    # apiUrl = "westcentralus.api.cognitive.microsoft.com"
-    # uri = "https://" + apiUrl
-    #
-    # conn = Faraday::Connection.new(:url => uri) do |builder|
-    #  ## URLをエンコードする
-    #   builder.use Faraday::Request::UrlEncoded
-    #  ## ログを標準出力に出したい時(本番はコメントアウトでいいかも)
-    #   builder.use Faraday::Response::Logger
-    #  ## アダプター選択（選択肢は他にもあり）
-    #   builder.use Faraday::Adapter::NetHttp
-    #
-    #   #microsoft vision apiKey
-    #   category = "Categories,Description,Tags,Faces&details=Landmarks"
-    #   requrl = "/vision/v1.0/analyze" + "?visualFeatures=" + category
-    #
-    #   res = conn.post do |req|
-    #      req.url requrl
-    #      req.headers['Content-Type'] = 'application/json'
-    #      req.headers['Ocp-Apim-Subscription-Key'] = '4892073d721d4314844a1e0e618d399e'
-    #      req.body = '{"url":"https://blog-001.west.edge.storage-yahoo.jp/res/blog-89-aa/chi316shun/folder/749549/46/20120746/img_0"}'
-    #   end
-    #
-    # end
+    conn = Faraday::Connection.new(:url => uri) do |builder|
+     ## URLをエンコードする
+      builder.use Faraday::Request::UrlEncoded
+     ## ログを標準出力に出したい時(本番はコメントアウトでいいかも)
+      builder.use Faraday::Response::Logger
+     ## アダプター選択（選択肢は他にもあり）
+      builder.use Faraday::Adapter::NetHttp
+
+
+      res = conn.post do |req|
+         req.url lineUrl
+         req.headers['Content-Type'] = 'application/json'
+         req.headers['Authorization'] = 'Bearer '+CAT_LINE
+         req.body = json: {:"to" => user_id, :"messages" => text}
+      end
+
     # render json: {"user_id": user_id}, status: 200
-    render json: user_id, status: 200
+    render status: 200
   end
 
   def handle_webhook
