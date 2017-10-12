7d16455b33187cb0ee7fdd3d72512ed7dc73745d
Kazuya <fjky9132@gmail.com>
2017-10-12 14:52:42 -0700

    Initialize repository

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,11 +5,13 @@ class HomeController < ApplicationController
   def line_test
     print params
 
-    
+
       # messaging = params['events'][0]['type']
       # raise "messaging is needed" unless messaging
-      user_id = params['events'][0]['source']['userId']
-      print user_id
+
+      # user_id = params['events'][0]['source']['userId']
+      # print user_id
+
       # raise "line user id is needed" unless user_id
 
     # rescue => e
@@ -46,9 +48,8 @@ class HomeController < ApplicationController
   end
 
   def handle_webhook
-    ##########################################
+
     # faradayコネクションの作成
-    ##########################################
 
     # ms Vision api url
     apiUrl = "westcentralus.api.cognitive.microsoft.com"
@@ -64,9 +65,7 @@ class HomeController < ApplicationController
 
     end
 
-    ##########################################
     # faradayからpostリクエスト
-    ##########################################
 
     #microsoft vision apiKey
     category = "Categories,Description,Tags,Faces&details=Landmarks"
